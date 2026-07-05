9f3c3b80f362ad1b24ba6d6ae72c790eeeff7a1d
Yash Bele <yashganrajbele@gmail.com>
2026-07-05 10:48:33 +0530

    Update GoogleLoginButton.jsx

--- frontend/src/features/auth/components/GoogleLoginButton.jsx
+++ frontend/src/features/auth/components/GoogleLoginButton.jsx
@@ -5,9 +5,9 @@ import { Spinner } from "@/components/ui/spinner";
 export default function GoogleLoginButton() {
   const [googleLogin, { isLoading }] = useGoogleLoginMutation();
   return (
-    <div className="h-9 w-50">
+    <div className="h-9">
       {isLoading ? (
-        <div className="h-full w-full flex items-center justify-center rounded-full bg-black/50 text-white">
+        <div className="h-full w-50 flex items-center justify-center rounded-full bg-black/50 text-white">
           <span>Google</span> <Spinner className="ml-1" />
         </div>
       ) : (
